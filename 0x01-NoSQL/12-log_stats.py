adc713ba59057a8d479841f3bf5da8a8d2048bea
Damadet <adedokundamilare41@gmail.com>
2023-08-15 22:30:18 +0200

    No sql

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,47 +1,24 @@
 #!/usr/bin/env python3
-"""
-Script that provides some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx
-first line: x logs, where x is the number of documents in this collection
-second line: Methods:
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
-"""
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
 
 
-global METHODS
-
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-
-def log_stats_method(mongo_collection, method):
-    """
-    Prototype: def log_stats_method(mongo_collection, method):
-    Provide some stats about some specific http methods in the log
-    """
-    value = mongo_collection.count_documents(
-        {"method": {"$regex": method}}
-    )
-    print(f"\tmethod {method}: {value}")
-    return
-
-
-def log_stats(mongo_collection):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
-    """
-    total = mongo_collection.count_documents({})
-    print(f"{total} logs")
-    print("Methods:")
-
-    for method in METHODS:
-        log_stats_method(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
-
-
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    """ Provides some stats about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    n_logs = nginx_collection.count_documents({})
+    print(f'{n_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
